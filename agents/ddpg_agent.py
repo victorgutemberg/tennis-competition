5b9c4cbef289cdd8897b2ec2fb23f7cfef042dfc
Victor Gutemberg Oliveira Marques <victorgo.marques@gmail.com>
2021-05-27 15:33:32 -0700

    Adding trained agent

--- agents/ddpg_agent.py
+++ agents/ddpg_agent.py
@@ -9,13 +9,13 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 512        # minibatch size
 GAMMA = 0.99            # discount factor
-TAU = 2e-1              # for soft update of target parameters
+TAU = 1e-2              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
 LR_CRITIC = 1e-3        # learning rate of the critic
-WEIGHT_DECAY = 0.0000     # L2 weight decay
-BATCH_SIZE = 512         # minibatch size
-BUFFER_SIZE = int(1e5)  # replay buffer size
+WEIGHT_DECAY = 0        # L2 weight decay
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
